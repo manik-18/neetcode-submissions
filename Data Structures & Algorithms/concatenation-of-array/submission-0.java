7ee576c79676c973843e7c12585c06547aeeb069
Manik Gupta <guptamanik1810@gmail.com>
2026-09-12 22:19:53 +0530

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int ans[] = new int[2*nums.length];
+        for(int i=0;i<ans.length;i++){
+            
+            if(i>nums.length-1){
+                ans[i]=nums[i-nums.length];
+            }
+            else{
+                ans[i]=nums[i];
+            }
+        }
+        return ans;
+    }
+}
